9bddbe15d2b5655e446c5b78e1a6289fde070146
Mikhail <a104s@mail.ru>
2022-11-29 18:44:19 +0300

    next commit

--- Modul_8_hw/game.py
+++ Modul_8_hw/game.py
@@ -1,22 +1,52 @@
-''' Игра угадай число'''
+""" Игра угадай число:
+    Компьютер сам загадывает и сам угадывает число
+"""
+
 import numpy as np
 
-num = np.random.randint(1,101)
+def find_number(number: int = 1) -> int:
+    """Угадываем число
 
-count = 0
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
 
-while True:
-    count += 1
-    pred_num = int(input('Угадай число от 1 до 100'))    
-    
-    if pred_num > num:
-        print('Number must be lower')
-    
-    elif pred_num < num:
-        print('Number must be higher')
-    else:
-        print(f'You have found a number {num} for {count} attempts')
-        break
-    
-    
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    low = 0
+    high = 101
+    while True:
+        count += 1
+        predict_number = (low + high)//2       # Угадываем бинарным поиском
+        if number > predict_number:
+            low = predict_number
+        elif number < predict_number:
+            high = predict_number 
+        else:
+            break                              # выход из цикла если угадали
+    return count
+
+
+def score_game(find_number) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        find_number([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    #print(random_array)
+    for number in random_array:
+        count_ls.append(find_number(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+if __name__ == "__main__":
+    score_game(find_number)
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# sf_data_science
-SF Homework
+# My Data Science Projects
+
+From the [Skillfactory Data Science course](https://skillfactory.ru/data-scientist-pro)
+
+## Проекты
+
+* [Проект 0. Игра: Отгадай число] (https://github.com/PapylevMN/sf_data_science/tree/main/Modul_8_hw)
+* [Проект 1. __________] (____________)
+* [Проект 2. __________] (____________)
